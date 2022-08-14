02894efdf2069a79559fc52e0a263b4235afcb45
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-08-14 21:49:54 +0900

    08 14 로그인 제한

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,28 +1,65 @@
 /* 라이브러리 */
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 /* SASS */
 import '../../styles/Login.scss';
 
 function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value)
+    const [disabled, setDisalbed] = useState(true)
+    const onEmailHandler = (e) => {
+        setEmail({email: e.currentTarget.value})
     }
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value)
+    const onPasswordHandler = (e) => {
+        setPassword({password: e.currentTarget.value})
+    }
+    // const onActiveButton = () => {
+    //   state.email.includes("@") && state.password.length >= 8
+    //   ? 
+    //   email: "",
+    //   password: "",
+    //   isActive: false
+    // }
+    const onSubmitHandler = () => {
+      axios.post("/auth/signin", {
+        email: email,
+        password: password 
+      }).then(function(response){
+        console.log('성공');
+      }).catch(function(error){
+        console.log(error);
+      });
     }
     return (
       <>
-        <form className="login-form">
-          <label>이메일</label>
-            <input type="email" value={email} onChange={onEmailHandler}/>
+        <div className="login-form-wrapper">
+          <h3>로그인</h3>
+          <form className="login-form">
+            <label>이메일</label>
+            <input 
+            type="email" 
+            value={email} 
+            onChange={onEmailHandler}
+            placeholder='이메일을 입력하세요.'/>
             <label>비밀번호</label>
-            <input type="password" value={password} onChange={onPasswordHandler}/>
-            <button>로그인</button>
+
+            <input 
+            type="password" 
+            value={password} 
+            onChange={onPasswordHandler}
+            placeholder='비밀번호를 입력하세요.'/>
+
+            <button
+            className={disabled ? 'disabledBtn' : ''}
+            type="submit"
+            onClick={onSubmitHandler}
+            disabled={disabled}>로그인</button>
+
             <Link to="/register"><p className="btn-go-register">아직 회원가입이 안 되어 있으신가요?</p></Link>
-        </form>
+          </form>
+        </div>
       </>
     )
 }
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,7 @@
 /* 라이브러리 */
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 /* SASS */
 import '../../styles/Register.scss';
 
@@ -21,20 +22,38 @@ function Register() {
   const onPasswordConfigHandler = (event) => {
     setPasswordConfig(event.currentTarget.value)
   }
+  const onSubmitHandler = () => {
+    axios.post("/auth/signup", {
+      email: email,
+      password: password 
+    }).then(function(response){
+      console.log('성공');
+    }).catch(function(error){
+      console.log(error);
+    });
+  }
+  
   return (
     <>
-      <form className="register-form">
-        <label>이름</label>
-        <input type="text" value={name} onChange={onNameHandler}/>
-        <label>이메일</label>
-        <input type="email" value={email} onChange={onEmailHandler}/>
-        <label>비밀번호</label>
-        <input type="password" value={password} onChange={onPasswordHandler}/>
-        <label>비밀번호 확인</label>
-        <input type="password" value={passwordConfig} onChange={onPasswordConfigHandler}/>
-        <button>회원가입</button>
-        <Link to="/"><p className="btn-go-login">가입이 되어 있으신가요?</p></Link>
-      </form>
+      <div className="register-form-wrapper">
+        <h3>회원가입</h3>
+        <form className="register-form">
+          <label>이름</label>
+          <input type="text" value={name} onChange={onNameHandler} 
+          placeholder='이름을 입력하세요.'/>
+          <label>이메일</label>
+          <input type="email" value={email} onChange={onEmailHandler} 
+          placeholder='이메일을 입력하세요.'/>
+          <label>비밀번호</label>
+          <input type="password" value={password} onChange={onPasswordHandler} 
+          placeholder='비밀번호는 8자 이상 설정하세요.'/>
+          <label>비밀번호 확인</label>
+          <input type="password" value={passwordConfig} 
+          onChange={onPasswordConfigHandler} placeholder='비밀번호를 한 번 더 입력하세요.'/>
+          <button type="submit" onClick={onSubmitHandler}>회원가입</button>
+          <Link to="/"><p className="btn-go-login">가입이 되어 있으신가요?</p></Link>
+        </form>
+      </div>
     </>
   )
 }
